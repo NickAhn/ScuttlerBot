8b4e0c0467aac8c2e660a50975e378a4e4501d37
Nick Ahn <n_ahn2@u.pacific.edu>
2023-01-09 13:50:03 -0800

    Add RIOT API handler

---
+++ riot.py
@@ -0,0 +1,46 @@
+# Riot API handler
+import requests
+from pprint import pprint
+import secret
+
+api_key = secret.api_key
+
+HEADER = {
+    "X-Riot-Token": api_key
+}
+
+
+def getAccountById(summonerName: str) -> dict:
+    '''
+    Get Account info by Summoner Name
+    Params:
+        summonerName: str  = In Game Name
+    Return: dictionary with the following metadata:
+        accountId: str      = Encrypted account ID. Max length 56 characters.
+        profileIconId: int	= ID of the summoner icon associated with the summoner.
+        revisionDate: long  = Date summoner was last modified specified as epoch milliseconds. The following events will update this timestamp: summoner name change, summoner level change, or profile icon change.
+        name:str	        = Summoner name.
+        id:str          	= Encrypted summoner ID. Max length 63 characters.
+        puuid:str	        = Encrypted PUUID. Exact length of 78 characters.
+        summonerLevel:long	= Summoner level associated with the summoner.
+    '''
+    endpoint = f'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summonerName}'
+    res = requests.get(endpoint, headers=HEADER)
+    json_data = res.json()
+    return json_data
+
+
+def getSummonerDataByEncryptedId(encryptedSummonerId: str) -> dict:
+    '''
+    Get Detailed Summoner Data for Ranked Flex and Solo/Duo.
+    Params:
+        encryptedSummonerId: str    = 'id' value from getAccountById
+    Return:
+        json as dictionary with data such as Rank, Tier, Wins, Losses, etc...
+    '''
+    endpoint = f'https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/{encryptedSummonerId}'
+    res = requests.get(endpoint, HEADER)
+    return res.json()
+
+# pprint(getAccountById("NlCKEL"))
+# pprint(getSummonerDataByEncryptedId('v659wx8jEAW30CK5U1p18o3oqCF9pu94zVEY1p5Er0qWY5c'))
