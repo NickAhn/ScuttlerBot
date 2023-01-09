5c11e24974bbe2072ab8585e839f4bceddce6e47
Nick Ahn <n_ahn2@u.pacific.edu>
2023-01-09 15:10:01 -0800

    Add discord bot client

--- app.py
+++ app.py
@@ -1,3 +1,30 @@
+import discord
+import secret
+
+TOKEN = secret.disc_token
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print("{0.user} is Online".format(client))
+
+@client.event
+async def on_message(message):
+    username = str(message.author).split('#')[0]
+    user_message = str(message.content)
+    channel = str(message.channel.name)
+    print(f"{username}: {user_message} ({channel})")
+
+    if message.author == client:
+        return
+
+    if user_message.lower() == "test":
+        await message.channel.send(f"Hello {username}!")
+        return
+
 if __name__ == '__main__':
-    pass
+    client.run(TOKEN)
 
